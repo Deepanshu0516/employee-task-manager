6321ef1f32716facd95b233ba55fcb3501d2f92d
Deepanshu0516 <163700831+Deepanshu0516@users.noreply.github.com>
2026-08-10 11:06:11 +0530

    Change button click action to update task status to 'failed'

--- src/components/TaskList/FailedTask.jsx
+++ src/components/TaskList/FailedTask.jsx
@@ -11,7 +11,7 @@ const FailedTask = ({ data, index, updateStatus }) => {
       <p className='text-sm mt-2'>{data.taskDescription}</p>
       <div className='mt-4'>
         <button
-          onClick={() => updateStatus(index, 'active')}
+          onClick={() => updateStatus(index, 'failed')}
           className='w-full bg-red-500 hover:bg-red-600 transition-transform duration-200 hover:scale-105 py-1 px-2 text-sm rounded-sm'>Failed</button>
       </div>
     </div>
